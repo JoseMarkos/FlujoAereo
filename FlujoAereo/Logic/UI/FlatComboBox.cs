d9cb57a49e34980531b47ac309956fce645addc5
Marcos <jkmarcosb@gmail.com>
2020-03-06 00:47:13 -0600

    adding some flat elements

---
+++ FlujoAereo/Logic/UI/FlatComboBox.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Forms;
+
+namespace FlujoAereo.Logic.UI
+{
+    public partial class FlatComboBox : ComboBox
+    {
+        public FlatComboBox(string name, string text, int x, int y)
+        {
+            FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            FormattingEnabled = true;
+            Location = new System.Drawing.Point(x, y);
+            Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            Name = "combo" + name;
+            Size = new System.Drawing.Size(314, 25);
+            TabIndex = 1;
+            Text = text;
+        }
+    }
+}
---
+++ FlujoAereo/Logic/UI/FlatLabel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlujoAereo.Logic.UI
+{
+    public partial class FlatLabel : Label
+    {
+        public FlatLabel (string name, int x, int y)
+        {
+            AutoSize = true;
+            BackColor = System.Drawing.Color.Crimson;
+            Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            ForeColor = System.Drawing.Color.White;
+            Location = new System.Drawing.Point(x, y);
+            Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            Name = "label" + name;
+            Size = new System.Drawing.Size(0, 25);
+            TabIndex = 7;
+        }
+    }
+}
