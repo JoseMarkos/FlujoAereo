a70aad4b86793145cd2fcca712cbc5fbde67abbb
Marcos <jkmarcosb@gmail.com>
2020-03-05 22:50:29 -0600

    Add project files.

---
+++ FlujoAereo/Models/Aerolinea.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlujoAereo.Models
+{
+    public class Aerolinea
+    {
+        public int id { get; set; }
+        public string code { get; set; }
+        public string icao { get; set; }
+        public string iata { get; set; }
+        public string name { get; set; }
+        public string logo { get; set; }
+        public string country { get; set; }
+        // public Journal journal { get; set; }
+    }
+}
